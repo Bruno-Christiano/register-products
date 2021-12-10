398129691c1f52eeee9c812c381052f2850de64c
Bruno Santos <bruno_cris_santos@hotmail.com>
2021-12-10 17:51:23 -0400

    update(web-client): importando modulo http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentsModule } from './components/components.module';
 import { ViewsModule } from './views/views.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { ViewsModule } from './views/views.module';
     BrowserAnimationsModule,
     ComponentsModule,
     ViewsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
